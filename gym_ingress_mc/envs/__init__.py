30d99fc34729e0c6e8726d13c3bf5e8aa97bbc02
Chenzheng Wang <templarares@163.com>
2021-07-31 18:36:38 +0800

    corrected the file structure so this could be correctly imported

--- gym_ingress_mc/envs/__init__.py
+++ gym_ingress_mc/envs/__init__.py
@@ -1,6 +1,1 @@
-from setuptools import setup
-
-setup(name='gym_ingress_mc',
-      version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
-)
+from gym_ingress_mc.envs.ingress_env import IngressEnv
