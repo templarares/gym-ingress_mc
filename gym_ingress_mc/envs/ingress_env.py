4f30fd1286710a53294ffa4df64d0f356ba64c5e
Chenzheng Wang <templarares@163.com>
2021-11-30 16:08:16 +0800

    thanks to pierre, now the action can be read into startcb

--- gym_ingress_mc/envs/ingress_env.py
+++ gym_ingress_mc/envs/ingress_env.py
@@ -1,4 +1,6 @@
+from functools import partial
 import sys
+import time
 from numpy.core.fromnumeric import shape
 sys.path.append('/home/templarares/devel/src/bit-car-inout-controller/python/')
 import mc_rtc_rl
@@ -15,7 +17,7 @@ def do_nothing(name,controller):
 def done_callback(name, controller):
     #print("{} done, robot configuration: {}".format(name, controller.robot().q))
     pass
-def start_callback(name, controller):
+def start_callback(action, name, controller):
     print("{} starting to run".format(name))
     if (name=="IngressFSM::RightFootCloseToCar::LiftFoot"):
         config = mc_rtc_rl.Configuration()
@@ -47,13 +49,20 @@ class IngressEnv(gym.Env):
 
         #self.observation_space=
         #self.reset()
+        self.gc = mc_rtc_rl.GlobalController('mc_rtc.yaml')
+        self.gc.init()
         pass
     def step(self, action):
-        "update current fsm state"
+        "update the fsm state that is immediately to run"
+        "this is the preceding state"
+        prevState = self.gc.currentState()
+        "timer to count execution time of this step"
+        startTime=time.time()
         if (self.gc.currentState()=="IngressFSM::RightFootCloseToCar::LiftFoot"):
             pass
         self.gc.set_rlinterface_done_cb(do_nothing)
-        self.gc.set_rlinterface_start_cb(start_callback)
+        self.gc.set_rlinterface_start_cb(partial(start_callback, action))
+        #self.gc.set_rlinterface_start_cb(lambda name, controller: start_callback(action, name, controller))
         #self.currentFSMState = 
         "check if action is in current avaialbe action space"
         #assert ...
@@ -65,7 +74,6 @@ class IngressEnv(gym.Env):
         #while self.gc.running:
         "if previous state is done, proceed to next state by calling the fsm's next()"
         if (self.gc.ready()):
-            print("asdfasdfasdfasddf")
             self.gc.nextState()
             self.gc.run()
         "fsm state\'s teardown() is immediate followed by next state\'s start()"
@@ -74,15 +82,47 @@ class IngressEnv(gym.Env):
         while (self.gc.running and (not self.gc.ready())):            
             self.gc.run()
         #now the fsm is ready to proceed to the next state
-        print("Current state is: %s"%self.gc.currentState())
-        return [0,0,0],1,False,{}
+        "this is the state that has just finished execution"
+        currentState = self.gc.currentState()
+        "timer to keep track of execution of one step"
+        endTime = time.time()
+        # print("execution of this state is %f"%(endTime-startTime))
+        # print("Current state is: %s"%currentState)
+        # print("Current position of LeftFoot is:", self.gc.EF_trans("LeftFoot"))
+        # print("Current orientation of LeftFoot is:", self.gc.EF_rot("LeftFoot"))
+        #return observation, reward, done and info
+
+        #observation: location of COM, pose of four EFs
+        #reward: for grasping state, reward = inverse(distance between ef and bar)-time elapsed+stateDone, using the function from minDist.py
+        #done: 
+        #info: {}
+        done = False
+        "for completing a state, the reward is 10 by default; if the controller failed, the punishment is -100"
+        if (self.gc.running ):
+            reward = 10
+        else:
+            reward = -100
+            done = True
+
+        "if last state is done,done is True and reward+=100;also some states are more rewarding than others"
+        if (currentState=="IngressFSM::SitPrep"):
+            reward += 100
+            done = True
+        elif (currentState=="IngressFSM::RightFootCloseToCar"):
+            reward +=10
+        elif (currentState=="IngressFSM::AdjustCoM"):
+            reward += 20        
+
+        #reward function. currently for the gripping only;
+        return [0,0,0],reward,done,{}
 
     
     def reset(self):
-        #self.gc = mc_rtc_rl.GlobalController('/home/templarares/.config/mc_rtc/mc_rtc.yaml')
-        self.gc = mc_rtc_rl.GlobalController('mc_rtc.yaml')
+        #self.gc = mc_rtc_rl.GlobalController('/home/templarares/.config/mc_rtc/mc_rtc.yaml')       
         #self.gc.reset()
-        self.gc.init()
+        print("resetting gc...")
+        self.gc.reset()
+        #self.gc.init()
         pass
     def render (self, moder='human'):
         pass
