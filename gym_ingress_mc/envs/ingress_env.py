68c78cbb1df4e38e745b9f6548c571ecf686f05f
Chenzheng Wang <templarares@163.com>
2021-12-10 21:34:55 +0800

    converted this to use bit-xuanyuan robot and its corresponding bit-car-in-out controller

--- gym_ingress_mc/envs/ingress_env.py
+++ gym_ingress_mc/envs/ingress_env.py
@@ -11,6 +11,7 @@ from gym import error, spaces, utils
 from gym.spaces import Box
 from gym.utils import seeding
 from gym_ingress_mc.envs import helper
+import threading
 
 def do_nothing(name,controller):
     pass
@@ -19,7 +20,7 @@ def done_callback(name, controller):
     #print("{} done, robot configuration: {}".format(name, controller.robot().q))
     pass
 def start_callback(action, name, controller):
-    #print("{} starting to run".format(name))
+    # #print("{} starting to run".format(name))
     if (
         name=="IngressFSM::RightFootCloseToCarFSM::LiftFoot" 
      ):
@@ -37,6 +38,34 @@ def start_callback(action, name, controller):
         Completion2=com.add("completion")
         helper.EditTimeout(Completion2,action[9])
         return config
+    # elif(name=="IngressFSM::LeftHandToBar"):
+    #     config = mc_rtc_rl.Configuration()
+    #     tasks = config.add("tasks")
+    #     com = tasks.add("com")
+    #     com.add("weight",int(2000*(abs(action[0]))))
+    #     left_hand = tasks.add("left_hand")
+    #     left_hand.add("weight",int(2000*(abs(action[0]))))
+    #     target=left_hand.add("target")
+    #     target.add_array("rotation",np.concatenate([[0],action[1:4]*0.2])+[-0.0480502,0.844923,0.427402,0.317999])
+    #     target.add_array("translation",np.array(action[4:7]*0.2+[0.587457,0.59999,1.6436]))
+    #     left_hand.add("weight",int(2000*(abs(action[8]))))
+    #     Completion1=left_hand.add("completion")
+    #     helper.EditTimeout(Completion1,action[9])
+    #     return config
+    # elif(name == "IngressFSM::LeftHandGrip"):
+    #     config = mc_rtc_rl.Configuration()
+    #     tasks = config.add("tasks")
+    #     com = tasks.add("com")
+    #     com.add("weight",int(2000*(abs(action[0]))))
+    #     left_hand = tasks.add("left_hand")
+    #     left_hand.add("weight",int(2000*(abs(action[0]))))
+    #     target=left_hand.add("target")
+    #     target.add_array("rotation",np.concatenate([[0],action[1:4]*0.2])+[-0.0850695,0.869396,0.421597,0.243249])
+    #     target.add_array("translation",np.array(action[4:7]*0.2+[0.49871,0.55743,1.57202]))
+    #     left_hand.add("weight",int(2000*(abs(action[8]))))
+    #     Completion1=left_hand.add("completion")
+    #     helper.EditTimeout(Completion1,action[9])
+    #     return config
     elif(
         name=="IngressFSM::RightFootCloseToCarFSM::MoveFoot" or
         name=="IngressFSM::RightFootCloseToCarFSM::PutFoot"
@@ -58,18 +87,35 @@ def start_callback(action, name, controller):
     elif (name=="IngressFSM::CoMToRightFoot"):
         config = mc_rtc_rl.Configuration()
         tasks = config.add("tasks")
+        com=tasks.add("com")
+        com.add_array("move_com",np.array(action[1:4]*0.1+[-0.05,-0.20,0.0]))
+        com.add("weight",int(2000*(abs(action[8]))))
+        Completion1=com.add("completion")
+        helper.EditTimeout(Completion1,action[9])
+        return config
+    elif (name=="IngressFSM::LandHip" or 
+        name=="IngressFSM::LandHipPhase2"):
+        config = mc_rtc_rl.Configuration()
+        tasks = config.add("tasks")
+        com = tasks.add("com")
+        com.add("weight",int(2000*(abs(action[0]))))
         right_hip=tasks.add("right_hip")
-        target=right_hip.add("target")
-        target.add_array("position",np.array(action[0:3]*0.1+[-0.12,0.57,0.9]))
+        position=right_hip.add_array("position",action[1:4]*0.1+[-0.254778,0.845367,0.849587])
+        #target.add_array("rotation",np.array(action[1:4]))
+        right_hip_ori=tasks.add("right_hip_ori")
+        right_hip_ori.add_array("orientation",np.concatenate([[0],action[4:7]*0.2])+[0.370327,0.671353,0.150604,0.624068])
         right_hip.add("weight",int(2000*(abs(action[8]))))
+        right_hip_ori.add("weight",int(2000*(abs(action[8]))))
         Completion1=right_hip.add("completion")
         helper.EditTimeout(Completion1,action[9])
+        Completion2=right_hip_ori.add("completion")
+        helper.EditTimeout(Completion2,action[9])
         return config
     elif (name == "IngressFSM::AdjustCoM"):
         config = mc_rtc_rl.Configuration()
         tasks = config.add("tasks")
         com = tasks.add("com")
-        com.add_array("com",np.array(action[0:3]*0.1+[0.25, 0.1,0.95]))
+        com.add_array("com",np.array(action[1:4]*0.1+[0.0642257,-0.336135,0.916262]))
         com.add("weight",int(2000*(abs(action[8]))))
         Completion1=com.add("completion")
         helper.EditTimeout(Completion1,action[9])
@@ -100,22 +146,33 @@ class IngressEnv(gym.Env):
 
         #self.observation_space=
         #self.reset()
-        self.gc = mc_rtc_rl.GlobalController('mc_rtc.yaml')
-        self.gc.init()
+        #self.gc = mc_rtc_rl.GlobalController('mc_rtc.yaml')
+        #self.sim.gc().init()
+        self.mjvisual=False
+        self.sim=mc_rtc_rl.MjSim('mc_rtc.yaml', self.mjvisual)
+        #self.sim.gc.init()
+        "multi-threaded version for mc_mujoco rendering. doesn't seem to work"
+        # self.render_=True
+        # def render():
+        #     while True:
+        #         self.sim.updateScene()
+        #         render = self.sim.render()
+        # thread = threading.Thread(target = render)
+        # thread.start()
         pass
     def step(self, action):
         "update the fsm state that is immediately to run"
         "this is the preceding state"
-        prevState = self.gc.currentState()
+        prevState = self.sim.gc().currentState()
         "timer to count execution time of this step"
         "when step() is called for the first time, the next reset() will not be the 1st episode"
         self.isFirstEpisode=False
         #startTime=time.time()
-        if (self.gc.currentState()=="IngressFSM::RightFootCloseToCar::LiftFoot"):
+        if (self.sim.gc().currentState()=="IngressFSM::RightFootCloseToCar::LiftFoot"):
             pass
-        self.gc.set_rlinterface_done_cb(do_nothing)
-        self.gc.set_rlinterface_start_cb(partial(start_callback, action))
-        #self.gc.set_rlinterface_start_cb(lambda name, controller: start_callback(action, name, controller))
+        self.sim.gc().set_rlinterface_done_cb(do_nothing)
+        self.sim.gc().set_rlinterface_start_cb(partial(start_callback, action))
+        #self.sim.gc().set_rlinterface_start_cb(lambda name, controller: start_callback(action, name, controller))
         #self.currentFSMState = 
         "check if action is in current avaialbe action space"
         #assert ...
@@ -124,33 +181,46 @@ class IngressEnv(gym.Env):
         "call the reward function"
 
         "advance mc_rtc until the next fsm state or gc failure"
-        #while self.gc.running:
+        #while self.sim.gc().running:
         "if previous state is done, proceed to next state by calling the fsm's next()"
-        if (self.gc.ready()):
-            self.gc.nextState()
-            self.gc.run()
+        if (self.sim.gc().ready()):
+            self.sim.gc().nextState()
+            self.sim.stepSimulation()
+            """gc().run() guarantees to make gc().nextState() false, which is necessary for proceeding to the next state; 
+            Yet sim.stepSimulation() can't do that. So I have to put gc().run() here. Doesn' seem right, but it does the trick"""
+            self.sim.gc().run()
         "fsm state\'s teardown() is immediate followed by next state\'s start()"
         "so use the fsm executor's ready() to check stateDone and forget about done_cb"        
-        #while (self.gc.running):        
-        while (self.gc.running and (not self.gc.ready())):            
-            self.gc.run()
+        #while (self.sim.gc().running):
+        iter_=0
+        render_=True
+        # print("gc is running:%s"%self.sim.gc().running)
+        # print("gc is ready:%s"%self.sim.gc().ready())        
+        while (self.sim.gc().running and render_ and (not self.sim.gc().ready())):  
+            self.sim.stepSimulation()
+            if(self.mjvisual):
+                if iter_ % 50 == 0:
+                    self.sim.updateScene()
+                    render_ = self.sim.render()            
+                iter_+=1
+            #print(iter_)
         #now the fsm is ready to proceed to the next state
         "this is the state that has just finished execution"
-        currentState = self.gc.currentState()
+        currentState = self.sim.gc().currentState()
         "timer to keep track of execution of one step"
         #endTime = time.time()
         # print("execution of this state is %f"%(endTime-startTime))
-        # print("Current state is: %s"%currentState)
-        # print("Current position of LeftFoot is:", self.gc.EF_trans("LeftFoot"))
-        # print("Current orientation of LeftFoot is:", self.gc.EF_rot("LeftFoot"))
+        # print("Current state is: %s"%(currentState))
+        # print("Current position of LeftFoot is:", self.sim.gc().EF_trans("LeftFoot"))
+        # print("Current orientation of LeftFoot is:", self.sim.gc().EF_rot("LeftFoot"))
         #return observation, reward, done and info
 
         #observation: location of COM, pose of four EFs, and state number
-        LHpose=np.concatenate([self.gc.EF_rot("LeftGripper"),self.gc.EF_trans("LeftGripper")])
-        RHpose=np.concatenate([self.gc.EF_rot("RightGripper"),self.gc.EF_trans("RightGripper")])
-        LFpose=np.concatenate([self.gc.EF_rot("LeftFoot"),self.gc.EF_trans("LeftFoot")])
-        RFpose=np.concatenate([self.gc.EF_rot("RightFoot"),self.gc.EF_trans("RightFoot")])
-        com=self.gc.com()
+        LHpose=np.concatenate([self.sim.gc().EF_rot("LeftGripper"),self.sim.gc().EF_trans("LeftGripper")])
+        RHpose=np.concatenate([self.sim.gc().EF_rot("RightGripper"),self.sim.gc().EF_trans("RightGripper")])
+        LFpose=np.concatenate([self.sim.gc().EF_rot("LeftFoot"),self.sim.gc().EF_trans("LeftFoot")])
+        RFpose=np.concatenate([self.sim.gc().EF_rot("RightFoot"),self.sim.gc().EF_trans("RightFoot")])
+        com=self.sim.gc().com()
         stateNumber=np.concatenate([[helper.StateNumber(name=currentState)],[]])
         observationd=np.concatenate([LHpose,RHpose,LFpose,RFpose,com,stateNumber])
         observation = observationd.astype(np.float32)
@@ -159,47 +229,52 @@ class IngressEnv(gym.Env):
         #info: {}
         done = False
         "for completing a state, the reward is 10 by default; if the controller failed, the punishment is -100"
-        if (self.gc.running ):
-            reward = 30
+        if (self.sim.gc().running and render_==True):
+            reward = 50
         else:
             reward = -200
             done = True
         "negative reward for time elapsed"
-        reward-=self.gc.duration()*10.0
+        reward-=self.sim.gc().duration()*10.0
 
         "if last state is done,done is True and reward+=100;also some states are more rewarding than others"
         if (currentState=="IngressFSM::SitPrep"):
             reward += 500
             done = True
         elif (currentState=="IngressFSM::RightFootCloseToCar"):
-            reward +=10
+            reward +=50
         elif (currentState=="IngressFSM::AdjustCoM"):
-            reward += 20     
+            reward += 50    
         elif (currentState=="IngressFSM::PutLeftFoot"):
             reward += 50
         "ADD HERE: use real robot's com, etc, to determine if it has failed; also calculate an extra reward term maybe?"
         "e.g. if (com_actual.z<0.5): reward -= 200 ; done = True"
-        if (self.gc.real_com()[2]<0.6):
+        if ((not done) and self.sim.gc().real_com()[2]<0.6):
             done = True
             reward -=200 
         #reward function. currently for the gripping only;
+        # print("current episode is done (finished or fatal failure): %s"%done)
         return observation,float(reward),done,{}
 
     
     def reset(self):
         "for demonstration purpose, no randomization at initial pose for the 1st episode"
         if (self.isFirstEpisode):
-            self.gc.reset()
+            """the gc().reset() shouldn't be here. but for now it is necessary"""
+            self.sim.reset()
+            self.sim.gc().reset()
         else:
-            self.gc.reset_random()
-        LHpose=np.concatenate([self.gc.EF_rot("LeftGripper"),self.gc.EF_trans("LeftGripper")])
-        RHpose=np.concatenate([self.gc.EF_rot("RightGripper"),self.gc.EF_trans("RightGripper")])
-        LFpose=np.concatenate([self.gc.EF_rot("LeftFoot"),self.gc.EF_trans("LeftFoot")])
-        RFpose=np.concatenate([self.gc.EF_rot("RightFoot"),self.gc.EF_trans("RightFoot")])
-        com=self.gc.com()
+            """the gc().reset_random() shouldn't be here. but for now it is necessary"""
+            self.sim.reset()
+            self.sim.gc().reset_random()
+        LHpose=np.concatenate([self.sim.gc().EF_rot("LeftGripper"),self.sim.gc().EF_trans("LeftGripper")])
+        RHpose=np.concatenate([self.sim.gc().EF_rot("RightGripper"),self.sim.gc().EF_trans("RightGripper")])
+        LFpose=np.concatenate([self.sim.gc().EF_rot("LeftFoot"),self.sim.gc().EF_trans("LeftFoot")])
+        RFpose=np.concatenate([self.sim.gc().EF_rot("RightFoot"),self.sim.gc().EF_trans("RightFoot")])
+        com=self.sim.gc().com()
         observationd=np.concatenate([LHpose,RHpose,LFpose,RFpose,com,[-1.0]])
         observation = observationd.astype(np.float32)
-        #self.gc.init()
+        #self.sim.gc().init()
         return observation
     def render (self, moder='human'):
         pass
